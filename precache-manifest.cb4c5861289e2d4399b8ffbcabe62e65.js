941c30bf19507a7a1ef9ef70ce057f3be4b5e791
egerton <egertonduring@yahoo.com>
2021-10-20 15:17:43 -0400

    Updates

---
+++ precache-manifest.cb4c5861289e2d4399b8ffbcabe62e65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5c6b47216d199acf2e5b07d11508a2",
+    "url": "/react-wise-spender/index.html"
+  },
+  {
+    "revision": "631b27ae8b5770331764",
+    "url": "/react-wise-spender/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "d216fdfbe6da8e33f528",
+    "url": "/react-wise-spender/static/css/main.a06a1c4e.chunk.css"
+  },
+  {
+    "revision": "631b27ae8b5770331764",
+    "url": "/react-wise-spender/static/js/2.df7df694.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-wise-spender/static/js/2.df7df694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48df2efe43cd3308f48",
+    "url": "/react-wise-spender/static/js/3.eae214ca.chunk.js"
+  },
+  {
+    "revision": "d216fdfbe6da8e33f528",
+    "url": "/react-wise-spender/static/js/main.de0e88ed.chunk.js"
+  },
+  {
+    "revision": "31885e06e8b92d602b4e",
+    "url": "/react-wise-spender/static/js/runtime-main.60274761.js"
+  }
+]);
